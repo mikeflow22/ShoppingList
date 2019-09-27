2a1e0d178db03c64bf5823c02419c82b177494e8
Michael R.F <mrflowers22@uwalumni.com>
2019-09-27 09:25:13 -0600

    created an extension for Shoppinglist

---
+++ ShoppingList/Model/ShoppingList+Convenience.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingList+Convenience.swift
+//  ShoppingList
+//
+//  Created by Michael Flowers on 9/27/19.
+//  Copyright © 2019 Michael Flowers. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+extension ShoppingList {
+    convenience init(name: String, items: [Item] = [], context: NSManagedObjectContext = CoreDataStack.mainContext) {
+        self.init(context: context)
+        self.name = name
+    }
+}
