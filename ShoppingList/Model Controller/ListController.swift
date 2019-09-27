bac4c6323e5c6feaecf6be1c1daf37b8209edd0e
Michael R.F <mrflowers22@uwalumni.com>
2019-09-27 09:45:25 -0600

    created fetchResults Controller for the ListController

--- ShoppingList/Model Controller/ListController.swift
+++ ShoppingList/Model Controller/ListController.swift
@@ -7,14 +7,49 @@
 //
 
 import Foundation
+import CoreData
 
 class ListController {
     
     static let shared = ListController()
-    
+    var fetchedResultsController: NSFetchedResultsController<List>
     init(){
         //create your fetchResultsController here
+        let fetchRequest: NSFetchRequest<List> = List.fetchRequest()
+        let nameSort = NSSortDescriptor(key: "name", ascending: true)
+        fetchRequest.sortDescriptors = [nameSort]
+        let resultsFetched  = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: CoreDataStack.mainContext, sectionNameKeyPath: nil, cacheName: nil)
+        
+        do {
+            try resultsFetched.performFetch()
+        } catch  {
+            print("Error  trying to fetch results: \(error)")
+        }
+        
+        fetchedResultsController =  resultsFetched
     }
     
-    func createList(
+    func createList(name: String) {
+        _ = List(name: name)
+        saveToPersistentStore()
+    }
+    
+    func delete(list: List){
+        CoreDataStack.mainContext.delete(list)
+        saveToPersistentStore()
+    }
+    
+    func update(list: List, withName name: String){
+        list.name = name
+        saveToPersistentStore()
+    }
+    
+    func saveToPersistentStore(){
+        do {
+            try CoreDataStack.mainContext.save()
+        } catch  {
+            print("Error saving to persistent store: \(error) in \(#function)")
+        }
+    }
+    
 }
