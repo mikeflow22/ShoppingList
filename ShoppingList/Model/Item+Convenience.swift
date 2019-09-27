5c2ec95a7bc9f2af383890f9d1a39e07e16fd3f9
Michael R.F <mrflowers22@uwalumni.com>
2019-09-27 09:29:49 -0600

    created extension for Item

---
+++ ShoppingList/Model/Item+Convenience.swift
@@ -0,0 +1,20 @@
+//
+//  Item+Convenience.swift
+//  ShoppingList
+//
+//  Created by Michael Flowers on 9/27/19.
+//  Copyright © 2019 Michael Flowers. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+extension Item {
+    
+    //If we are saying  you  cannot create an item without a shopping list, why give it context default value here? Shouldn't the context be the same as the shoppinglist?
+    convenience init(name: String, isComplete: Bool = false, shoppingList: ShoppingList, context: NSManagedObjectContext = CoreDataStack.mainContext) {
+        self.init(context: context)
+        self.name = name
+        self.shoppingList = shoppingList
+    }
+}
